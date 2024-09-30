b73e5552d1a469f55425422d0822e6040f3032be
Amit Dutta <amitdutta2406@gmail.com>
2024-09-30 12:30:17 +0530

    Updated Mentor Showcase component

--- components/MentorShowCase.tsx
+++ components/MentorShowCase.tsx
@@ -13,7 +13,7 @@ import {
     CarouselPrevious,
 } from "@/components/ui/carousel"
 import { GraduationCap, MessageSquare } from "lucide-react";
-import MentorCard from "@/components/MentorCard"
+import MentorCard from "@/components/MentorCard";
 
 
 interface Mentor {
@@ -25,28 +25,6 @@ interface Mentor {
     reviews: string;
     imageUrl: string;
 }
-
-const MentorCard = ({ mentor }: { mentor: Mentor }) => (
-    <Card className="shadow-lg">
-        <CardHeader className="p-0">
-            <img src={mentor.imageUrl} alt={mentor.name} className="w-full h-80 object-cover rounded-t-lg" />
-        </CardHeader>
-        <CardContent className="p-4 mt-4">
-            <CardTitle className="text-lg font-semibold mb-2">{mentor.name}</CardTitle>
-            <div className="text-sm text-gray-600 space-y-1">
-                <div className="flex items-center">
-                    <GraduationCap className="w-4 h-4 mr-2" />
-                    <span>{`${mentor.year}, ${mentor.college}`}</span>
-                </div>
-                <div>{mentor.major}</div>
-                <div className="flex items-center">
-                    <MessageSquare className="w-4 h-4 mr-2" />
-                    <span>{`${mentor.sessions} (${mentor.reviews})`}</span>
-                </div>
-            </div>
-        </CardContent>
-    </Card>
-);
 
 const MentorShowcase = ({ mentors, number }: { mentors: Mentor[], number: number }) => {
     return (
